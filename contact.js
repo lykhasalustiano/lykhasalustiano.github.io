50880358b0d014dc20d67844cce328aa6bd2d43a
lykhasalustinao <lykhasalustiano@gmail.com>
2026-04-14 21:30:21 +0800

    :coffin:

--- contact.js
+++ contact.js
@@ -1,11 +1,11 @@
-emailjs.init("YOUR_PUBLIC_KEY");
+emailjs.init("OdHFi_Pn_k_JDa--S");
 
 function sendEmail() {
   const btn = document.getElementById('send-btn');
   btn.textContent = 'Sending...';
   btn.disabled = true;
 
-  emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', '#contact-form')
+  emailjs.sendForm('service_47t8crr', 'template_m7shxqd', '#contact-form')
     .then(() => {
       btn.textContent = 'Sent!';
       document.getElementById('contact-form').reset();
